d178689c39571a14593ec20e2bbfcf010eb3cedd
farezhalim <farez@ualberta.ca>
2019-04-12 12:48:42 -0600

    housekeeping

---
+++ testing/software/archive/Test Codes/Send_and_Receive_Test/Send_and_Receive_Test.ino
@@ -0,0 +1,68 @@
+/**************************
+ *         Author         *
+ *      omarCartera       *
+ *                        *
+ *        5/8/2017        *
+ *                        *
+ * c.omargamal@gmail.com  *
+ **************************/
+
+#include <SPI.h>          //SPI is used to talk to the CAN Controller
+#include <mcp_can.h>
+
+MCP_CAN CAN(10);          //set SPI Chip Select to pin 10
+
+unsigned char len = 0;
+unsigned char buf[8];
+unsigned int canID;
+
+void setup()
+{
+  Serial.begin(115200);   //to communicate with Serial monitor
+
+//tries to initialize, if failed --> it will loop here for ever
+START_INIT:
+
+    if(CAN_OK == CAN.begin(CAN_500KBPS))      //setting CAN baud rate to 500Kbps
+    {
+        Serial.println("CAN BUS Shield init ok!");
+    }
+    else
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println("Init CAN BUS Shield again");
+        delay(100);
+        goto START_INIT;
+    }
+}
+
+//loading the data bytes of the message. Up to 8 bytes
+//unsigned char data[8] = {};
+unsigned char data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0FF};
+
+void loop()
+{
+    //This is from the CAN_Send
+    //CAN.sendMsgBuf(msg ID, extended?, #of data bytes, data array);
+    CAN.sendMsgBuf(0xFF, 0, 8, data);
+    delay(2000);    
+
+    //This is from the CAN_Receive
+    if(CAN_MSGAVAIL == CAN.checkReceive())    //check if data is coming
+    {
+        CAN.readMsgBuf(&len, buf);    //read data,  len: data length, buf: data buffer
+        canID = CAN.getCanId();       //getting the ID of the incoming message
+
+        Serial.print("ID is: ");
+        Serial.print(canID, HEX);     //printing the ID in its standard form, HEX
+
+        Serial.print("    Length is: ");
+        Serial.println(len);
+        
+        for(int i = 0; i<len; i++)    //looping on the incoming data to print them
+        {
+            Serial.write(buf[i]);     //Serial.write prints the character itself
+        }
+        Serial.println("\n\t*****************\n");
+    }
+}
